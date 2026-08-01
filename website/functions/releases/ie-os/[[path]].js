955497c165dae2f223b79df9c18077dcd66899e6
Jonas Siebler <sieblerjonas@gmail.com>
2026-08-01 09:38:41 +0200

    feat: add Pages Function for R2 release serving + fix blog merge marker

---
+++ website/functions/releases/ie-os/[[path]].js
@@ -0,0 +1,23 @@
+export async function onRequest(context) {
+  const { request, env } = context;
+  const url = new URL(request.url);
+  const path = url.pathname.replace(/^\/releases\/ie-os\//, '').replace(/^\//, '');
+
+  if (!path) {
+    return new Response('Not Found', { status: 404, headers: { 'content-type': 'text/plain' } });
+  }
+
+  const objectKey = `releases/ie-os/${path}`;
+  const object = await env.IE_OS_RELEASES.get(objectKey);
+
+  if (!object) {
+    return new Response('Not Found', { status: 404, headers: { 'content-type': 'text/plain' } });
+  }
+
+  const headers = new Headers();
+  headers.set('content-type', object.httpMetadata?.contentType || 'application/octet-stream');
+  headers.set('cache-control', 'public, max-age=3600');
+  headers.set('content-length', String(object.size));
+
+  return new Response(object.body, { headers });
+}
